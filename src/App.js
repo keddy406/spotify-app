1b985d18b449a8bf6994bb31d0253abe46339e43
kimi <keddy406@hotmail.com>
2020-10-03 23:09:48 +0800

    upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Login from "./Login";
+import "./App.css";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
+  React.useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
+      spotify.getUserPlaylists().then((playlists) =>
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists: playlists,
+        })
+      );
+    }
+
+    spotify.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
+      dispatch({
+        type: "SET_DISCOVER_WEEKLY",
+        discover_weekly: response,
+      })
+    );
+  }, []);
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Footer.css";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+
+import { Grid, Slider } from "@material-ui/core";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer_left">
+        <img
+          className="footer_albumLogo"
+          src="https://upload.wikimedia.org/wikipedia/commons/f/fc/USAAF_photo_of_Tokyo_after_the_10_March_air_raid.jpg"
+          alt=""
+        />
+        <div className="footer_songinfo">
+          <h4>Yeah</h4>
+          <p>User</p>
+        </div>
+      </div>
+      <div className="footer_center">
+        <ShuffleIcon className="footer_green" />
+        <SkipPreviousIcon className="footer_icon" />
+        <PlayCircleOutlineIcon className="footer_icon" fontSize="large" />
+        <SkipNextIcon className="footer_icon" />
+        <RepeatIcon className="footer_green" />
+      </div>
+
+      <div className="footer_right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
